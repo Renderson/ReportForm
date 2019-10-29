289079d1c1d707beeaeeaf8d429b95fb78b427ea
Renderson <renderson.silva@sodresantoro.com.br>
2019-10-29 13:29:18 -0300

    Commit implementation delete file PDF along with ID DB.

--- app/src/main/java/com/rendersoncs/reportform/view/MainActivity.java
+++ app/src/main/java/com/rendersoncs/reportform/view/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -47,7 +46,6 @@ import com.rendersoncs.reportform.util.GetInfoUserFirebase;
 import com.rendersoncs.reportform.util.RVEmptyObserver;
 import com.rendersoncs.reportform.util.SnackBarHelper;
 
-import java.io.File;
 import java.util.List;
 
 import butterknife.ButterKnife;
