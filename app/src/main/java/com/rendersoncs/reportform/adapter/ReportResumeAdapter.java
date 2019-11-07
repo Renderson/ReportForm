c58ae76095fbd2b80950fce2eebe10851551569b
Renderson <renderson.silva@sodresantoro.com.br>
2019-11-07 14:50:24 -0300

    Commit implementation new card in activity_report_resume_list, adjusted  ReportResumeAdapter for show and new card with photo.

--- app/src/main/java/com/rendersoncs/reportform/adapter/ReportResumeAdapter.java
+++ app/src/main/java/com/rendersoncs/reportform/adapter/ReportResumeAdapter.java
@@ -1,14 +1,20 @@
 package com.rendersoncs.reportform.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.rendersoncs.reportform.R;
 import com.rendersoncs.reportform.itens.ReportResumeItems;
 import com.rendersoncs.reportform.listener.OnItemListenerClicked;
@@ -20,6 +26,7 @@ public class ReportResumeAdapter extends RecyclerView.Adapter<ReportResumeAdapte
     private List<ReportResumeItems> repoResumeList;
 
     private OnItemListenerClicked onItemListenerClicked;
+    private Context context;
 
     public void setOnItemListenerClicked(OnItemListenerClicked onItemListenerClicked) {
         this.onItemListenerClicked = onItemListenerClicked;
@@ -27,6 +34,7 @@ public class ReportResumeAdapter extends RecyclerView.Adapter<ReportResumeAdapte
 
     public ReportResumeAdapter(List<ReportResumeItems> repoResumeList, Context context){
         this.repoResumeList = repoResumeList;
+        this.context = context;
     }
 
     @NonNull
@@ -48,6 +56,26 @@ public class ReportResumeAdapter extends RecyclerView.Adapter<ReportResumeAdapte
         holder.resume_note.setText(resumeItems.getNote());
 
         holder.resume_photo.setOnClickListener(view -> onItemListenerClicked.fullPhoto(position));
+
+        decorationItems(holder, resumeItems);
+    }
+
+    private void decorationItems(@NonNull ViewHolder holder, ReportResumeItems resumeItems) {
+        String image = resumeItems.getPhoto();
+        byte[] bytes = Base64.decode(image, Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        Glide.with(context).load(bitmap).centerCrop().into(holder.resume_photo);
+
+        String according = context.getResources().getString(R.string.according);
+        String notApplicable = context.getResources().getString(R.string.not_applicable);
+
+        if (holder.resume_conformity.getText().equals(according)){
+            holder.resume_conformity.setTextColor(ContextCompat.getColor(context, R.color.colorRadioC));
+        } else if (holder.resume_conformity.getText().equals(notApplicable)){
+            holder.resume_conformity.setTextColor(ContextCompat.getColor(context, R.color.colorRadioNA));
+        } else {
+            holder.resume_conformity.setTextColor(ContextCompat.getColor(context, R.color.colorRadioNC));
+        }
     }
 
     @Override
@@ -57,7 +85,8 @@ public class ReportResumeAdapter extends RecyclerView.Adapter<ReportResumeAdapte
 
     class ViewHolder extends RecyclerView.ViewHolder  {
 
-        private TextView resume_title, resume_description, resume_conformity, resume_note, resume_photo;
+        private TextView resume_title, resume_description, resume_conformity, resume_note;
+        private ImageView resume_photo;
 
         private ViewHolder(@NonNull View itemView) {
             super(itemView);
