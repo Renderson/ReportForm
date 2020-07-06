8e344bfd5f2831734b1e3612e0bcb8ec0c0d021c
Renderson <renderson-k19@hotmail.com>
2020-07-06 20:19:12 -0300

    Delete commits

--- app/src/main/java/com/rendersoncs/reportform/view/activitys/login/HomeActivity.kt
+++ app/src/main/java/com/rendersoncs/reportform/view/activitys/login/HomeActivity.kt
@@ -11,14 +11,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-
-        // For Version TEST
-        /* this.alertDialog.showDialog(HomeActivity.this,
-                "Versão de Teste!",
-                "Olá, você está usando uma versão de teste, com isso algumas coisas podem sair errado, tenha paciência e passe para nós o seu feed back.",
-                "ok",
-                (dialogInterface, i) -> { },
-                null, null, false);*/
     }
 
     fun callLogin(view: View?) {
