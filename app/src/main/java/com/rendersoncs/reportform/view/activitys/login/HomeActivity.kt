730f121ad54d637f5b673070d53873dc14cb980a
Renderson <renderson-k19@hotmail.com>
2020-07-07 23:14:56 -0300

    Delete code commit in HomeActivity

--- app/src/main/java/com/rendersoncs/reportform/view/activitys/login/HomeActivity.kt
+++ app/src/main/java/com/rendersoncs/reportform/view/activitys/login/HomeActivity.kt
@@ -7,18 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.rendersoncs.reportform.R
 
 class HomeActivity : AppCompatActivity() {
-    /*private AlertDialogUtil alertDialog = new AlertDialogUtil();*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        // For Version TEST
-        /* this.alertDialog.showDialog(HomeActivity.this,
-                "Versão de Teste!",
-                "Olá, você está usando uma versão de teste, com isso algumas coisas podem sair errado, tenha paciência e passe para nós o seu feed back.",
-                "ok",
-                (dialogInterface, i) -> { },
-                null, null, false);*/
     }
 
     fun callLogin(view: View?) {
