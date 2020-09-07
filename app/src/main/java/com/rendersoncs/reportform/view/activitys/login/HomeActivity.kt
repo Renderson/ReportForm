0c9d5ae459c316d4574785e3b1e96ff54a20eccb
Renderson <renderson-k19@hotmail.com>
2020-09-07 20:11:14 -0300

    implementation dark theme activity_welcome

--- app/src/main/java/com/rendersoncs/reportform/view/activitys/login/HomeActivity.kt
+++ app/src/main/java/com/rendersoncs/reportform/view/activitys/login/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.rendersoncs.reportform.R
@@ -17,6 +18,11 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
 
         preference = getSharedPreferences(ReportConstants.THEME.MY_PREFERENCE_THEME, Context.MODE_PRIVATE)
         val position = preference.getInt(ReportConstants.THEME.KEY_THEME, THEME_DAY)
