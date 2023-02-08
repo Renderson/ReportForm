ec7a98bab1e5acbd4c93156427aee8e0629f8e21
Renderson Cerqueira <renderson.silva@southsystem.com.br>
2023-02-08 19:09:16 -0300

    refactor SnackBarHelper.kt

--- app/src/main/java/com/rendersoncs/report/infrastructure/util/SnackBarHelper.kt
+++ app/src/main/java/com/rendersoncs/report/infrastructure/util/SnackBarHelper.kt
@@ -2,7 +2,9 @@ package com.rendersoncs.report.infrastructure.util
 
 import android.app.Activity
 import android.content.Context
-import android.os.Build
+import android.content.Intent
+import android.provider.Settings
+import android.view.View
 import android.view.ViewGroup.MarginLayoutParams
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
@@ -19,6 +21,21 @@ class SnackBarHelper {
         snk.show()
     }
 
+    fun showSnackBar(activity: Activity, view: View) {
+        val snackBar = Snackbar
+            .make(
+                view,
+                activity.getString(R.string.txt_check_networking),
+                Snackbar.LENGTH_LONG
+            )
+            .setAction(activity.resources.getString(R.string.check)) {
+                val intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                activity.startActivity(intent)
+            }
+        configSnackBar(activity, snackBar)
+        snackBar.show()
+    }
+
     companion object {
         @JvmStatic
         fun configSnackBar(context: Context, snack: Snackbar) {
@@ -29,17 +46,12 @@ class SnackBarHelper {
 
         private fun addMargins(snk: Snackbar) {
             val params = snk.view.layoutParams as MarginLayoutParams
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                params.setMargins(12, 12, 12, 12)
-            }
-            params.setMargins(8, 8, 8, 8)
+            params.setMargins(12, 12, 12, 12)
             snk.view.layoutParams = params
         }
 
         private fun setRoundBordersBg(context: Context, snk: Snackbar) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                snk.view.background = ContextCompat.getDrawable(context, R.drawable.bg_snackbar)
-            }
+            snk.view.background = ContextCompat.getDrawable(context, R.drawable.bg_snackbar)
         }
     }
 }
