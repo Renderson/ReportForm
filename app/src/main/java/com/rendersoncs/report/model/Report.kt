0a80dc310cd7afb123169c0b4d0ac6c314a06954
renderson <renderson.silva@southsystem.com>
2024-02-03 14:02:35 -0300

    lint code

--- app/src/main/java/com/rendersoncs/report/model/Report.kt
+++ app/src/main/java/com/rendersoncs/report/model/Report.kt
@@ -9,34 +9,34 @@ import java.util.*
 
 @Entity(tableName = "all_reports")
 data class Report(
-        @PrimaryKey(autoGenerate = true)
-        @ColumnInfo(name = "id")
-        var id: Int = 0,
-        @ColumnInfo(name = "company")
-        var company: String,
-        @ColumnInfo(name = "email")
-        var email: String,
-        @ColumnInfo(name = "date")
-        var date: String,
-        @ColumnInfo(name = "controller")
-        var controller: String,
-        @ColumnInfo(name = "score")
-        var score: String,
-        @ColumnInfo(name = "result")
-        var result: String,
-        @ColumnInfo(name = "concluded")
-        var concluded: Boolean? = false,
-        @ColumnInfo(name = "userId")
-        val userId: String
-): Serializable {
-        fun dateFormatter(): String {
-                val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-                val date = dateFormat.parse(date)
-                val formatter = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-                return formatter.format(date!!)
-        }
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+    @ColumnInfo(name = "company")
+    var company: String,
+    @ColumnInfo(name = "email")
+    var email: String,
+    @ColumnInfo(name = "date")
+    var date: String,
+    @ColumnInfo(name = "controller")
+    var controller: String,
+    @ColumnInfo(name = "score")
+    var score: String,
+    @ColumnInfo(name = "result")
+    var result: String,
+    @ColumnInfo(name = "concluded")
+    var concluded: Boolean? = false,
+    @ColumnInfo(name = "userId")
+    val userId: String
+) : Serializable {
+    fun dateFormatter(): String {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        val date = dateFormat.parse(date)
+        val formatter = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+        return formatter.format(date!!)
+    }
 
-        fun companyFormatter(): String {
-                return this.company.replace(" ", "-")
-        }
+    fun companyFormatter(): String {
+        return this.company.replace(" ", "-")
+    }
 }
