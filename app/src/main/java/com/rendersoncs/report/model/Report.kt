327dab49f13fe4fe75f1fc4fd2d7e5128b53c2ec
renderson <renderson.silva@southsystem.com>
2024-02-12 20:34:33 -0300

    properties with null

--- app/src/main/java/com/rendersoncs/report/model/Report.kt
+++ app/src/main/java/com/rendersoncs/report/model/Report.kt
@@ -11,32 +11,32 @@ import java.util.*
 data class Report(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
-    var id: Int = 0,
+    var id: Int? = null,
     @ColumnInfo(name = "company")
-    var company: String,
+    var company: String? = null,
     @ColumnInfo(name = "email")
-    var email: String,
+    var email: String? = null,
     @ColumnInfo(name = "date")
-    var date: String,
+    var date: String? = null,
     @ColumnInfo(name = "controller")
-    var controller: String,
+    var controller: String? = null,
     @ColumnInfo(name = "score")
-    var score: String,
+    var score: String? = null,
     @ColumnInfo(name = "result")
-    var result: String,
+    var result: String? = null,
     @ColumnInfo(name = "concluded")
     var concluded: Boolean? = false,
     @ColumnInfo(name = "userId")
-    val userId: String
+    val userId: String? = null
 ) : Serializable {
     fun dateFormatter(): String {
         val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-        val date = dateFormat.parse(date)
+        val date = dateFormat.parse(date ?: "")
         val formatter = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
         return formatter.format(date!!)
     }
 
     fun companyFormatter(): String {
-        return this.company.replace(" ", "-")
+        return this.company?.replace(" ", "-").toString()
     }
 }
